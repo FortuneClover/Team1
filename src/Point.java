f56a0331ffa23d8ed19674f1526bf3a779cd4a84
FortuneClover <a01077608653@pukyong.ac.kr>
2025-08-29 09:34:12 +0900

    Point.java edit

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,4 @@
-// Pointget 인터페이스를 구현하여 getX()와 getY() 메서드를 강제 구현합니다.
+// Pointset 인터페이스를 구현하여 getX()와 getY() 메서드를 강제 구현합니다.
 public class Point implements Pointset {
     // x, y 좌표는 외부에서 직접 변경할 수 없도록 private으로 선언하여 은닉(캡슐화)합니다.
     private double x;
